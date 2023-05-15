2393b74cf3c22c2087fde5d0f8bb520a27ae8adc
Roman Herasymenko <roman.herasymenko@gmail.com>
2023-05-15 18:06:59 +0200

    Added implementations for GobWriter and GobReader.

--- network_encoder.go
+++ network_encoder.go
@@ -1,5 +1,10 @@
 package network_encoder
 
+import (
+	"encoding/gob"
+	"io"
+)
+
 type INetworkWriter interface {
 	Write(source any) error
 }
@@ -11,4 +16,30 @@ type INetworkReader interface {
 type INetworkEncoder interface {
 	GetNetworkWriter() INetworkWriter
 	GetNetworkReader() INetworkReader
+}
+
+// GobWriter
+type GobWriter struct {
+	enc *gob.Encoder
+}
+
+func NewGobWriter(w io.Writer) *GobWriter {
+	return &GobWriter{enc: gob.NewEncoder(w)}
+}
+
+func (e *GobWriter) Write(source any) error {
+	return e.enc.Encode(source)
+}
+
+// GobReader
+type GobReader struct {
+	dec *gob.Decoder
+}
+
+func NewGobReader(r io.Reader) *GobReader {
+	return &GobReader{dec: gob.NewDecoder(r)}
+}
+
+func (reader *GobReader) Read(destination any) error {
+	return reader.dec.Decode(destination)
 }
