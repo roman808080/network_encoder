5467f9f7b4eef3c22349729526cee303a3ba4a82
Roman Herasymenko <roman.herasymenko@gmail.com>
2023-05-15 18:04:37 +0200

    Added interfaces.

--- network_encoder.go
+++ network_encoder.go
@@ -1,1 +1,14 @@
 package network_encoder
+
+type INetworkWriter interface {
+	Write(source any) error
+}
+
+type INetworkReader interface {
+	Write(source any) error
+}
+
+type INetworkEncoder interface {
+	GetNetworkWriter() INetworkWriter
+	GetNetworkReader() INetworkReader
+}
